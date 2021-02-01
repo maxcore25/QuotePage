39ff4474e543b488183249ad637f5e4999246e24
danya <den.martsinovich@yandex.ru>
2021-02-01 04:33:22 +0300

    post add done

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
+app.debug = True
 app.config['SECRET_KEY'] = 'sdjkfnsdjkfnsdfjsdkfnjdfsdnfj' # исправить потом!!!!
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:pass@localhost/quote_page_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+# Models of database
 
 
 class Post(db.Model):
@@ -17,16 +24,35 @@ class Post(db.Model):
     description = db.Column(db.String(1000), nullable=False)
     created_on = db.Column(db.DateTime(), default=datetime.utcnow)
 
+# Forms
+
+
+class PostForm(FlaskForm):
+    picture = StringField('Ссылка на картинку', validators=[DataRequired(message='Это обязательное поле')])
+    description = StringField('Описание', validators=[DataRequired(message='Это обязательное поле')])
+    submit = SubmitField('Опубликовать')
+
+
+db.create_all()
+
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    raw_posts = db.session.query(Post).all()
+    posts = [(el.im_link, el.description) for el in raw_posts]
+    return render_template('index.html', title='Главная', posts=posts)
 
 
 @app.route('/admin', methods=['GET', 'POST'])
 def admin():
-    return render_template('adminpage.html')
+    form = PostForm()
+    if form.validate_on_submit():
+        picture, description = form.picture.data, form.description.data
+        post = Post(im_link=picture, description=description)
+        db.session.add(post)
+        db.session.commit()
+    return render_template('adminpage.html', title='Добавление поста', form=form)
 
 
 if __name__ == '__main__':
